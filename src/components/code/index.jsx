6f7000c125ecde415ccef75e852a52e0305c4bfe
zhangmaolin <zml18735755791@163.com>
2023-04-14 17:33:48 +0800

    fix:代码编辑器

--- src/components/code/index.jsx
+++ src/components/code/index.jsx
@@ -2,20 +2,37 @@ import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-github";
-// import "ace-builds/src-noconflict/ext-language_tools";
+import { useState } from "react";
+import "ace-builds/src-noconflict/ext-language_tools";
 
 export default function Code() {
+  const [value, setValue] = useState();
   const onChange = (newValue) => {
-    console.log("change", newValue);
+    setValue(newValue);
   };
+
+  const onBlur = async () => {
+    try {
+      const res = await JSON.parse(value);
+      console.log(res);
+    }catch(e){
+      alert('输入格式不正确');
+    }
+    
+  }
   return (
     <div>
       <AceEditor
+        value={value}
         mode="json"
         theme="github"
+        enableBasicAutocompletion
+        enableLiveAutocompletion
+        enableSnippets
         fontSize={14}
-        readOnly={true}
+        // readOnly={true}
         onChange={onChange}
+        onBlur={onBlur}
         name="UNIQUE_ID_OF_DIV"
         editorProps={{ $blockScrolling: true }}
       />
